6f1735d8bb3b7e455f6e56a1f07a187f70584496
hamnord <h.nordenstein@hotmail.com>
2019-12-05 22:01:58 +0100

    added basic exercises

--- src/JavaExercises/BasicExercises.java
+++ src/JavaExercises/BasicExercises.java
@@ -4,6 +4,7 @@ public class BasicExercises {
 
   public static void main (String[] args){
 
+    /*
     System.out.println(" basic int exercise ");
     int myFirstNumber = (10 + 5) + (2 * 10);
     int mySecondNumber = 12;
@@ -11,7 +12,7 @@ public class BasicExercises {
     int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
     int myLastOne = 1000 - myTotal;
     System.out.println(" My Total: " + myTotal);
-    System.out.println(" My Last one: " + myLastOne);
+    System.out.println(" My Last one: " + myLastOne);*/
 
 
     System.out.println(" \n basic Primitive Types exercise");
@@ -50,6 +51,44 @@ public class BasicExercises {
 
     short bigShortLiteralValue = 32767;
 
+    System.out.println("Casting exercise");
+      int myTotal = (myMinIntValue / 2);
+      byte myNewByteValue = (byte) (myMinByteValue / 2);
+      short myNewShortValue = (short) (myMinShortValue / 2);
+
+    // prim type challenge
+    byte myByte = 10;
+    short myShort = 20;
+    int myInt = 30;
+    long sumWithLong = 50000L + 10L * (myByte + myShort + myInt);
+    System.out.println("Sum: " + sumWithLong);
+
+
+    System.out.println("Float and Double Exercise");
+
+    float myMinFloatValue = Float.MIN_VALUE;
+    float myMaxFloatValue = Float.MAX_VALUE;
+    System.out.println(" float min: " + myMinFloatValue);
+    System.out.println(" float Max: " + myMaxFloatValue);
+
+
+    double myMinDoubleValue = Double.MIN_VALUE;
+    double myMaxDoubleValue = Double.MAX_VALUE;
+    System.out.println(" Double min: " + myMinDoubleValue);
+    System.out.println(" Double Max: " + myMaxDoubleValue);
+
+    int myIntValue = 5 / 3;
+    float myFloatValue = 5f / 3f;
+    double myDoubleValue = 5d / 3d;
+
+    System.out.println("my Int: " + myIntValue);
+    System.out.println("my Float: " + myFloatValue);
+    System.out.println("my Double: " + myDoubleValue);
+
+    // double challenge
+    double pound = 10d;
+    double covertedTokilograms = pound * 0.45359237d;
+    System.out.println("Coverted to KG: " +covertedTokilograms);
 
   }
 
