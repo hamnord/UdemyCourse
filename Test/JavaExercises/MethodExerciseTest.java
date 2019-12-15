50dd77c6fa52f105c8f805ba3c731230c0ea6821
hamnord <h.nordenstein@hotmail.com>
2019-12-15 23:12:52 +0100

    added Test for udemyCourse

---
+++ Test/JavaExercises/MethodExerciseTest.java
@@ -0,0 +1,17 @@
+package JavaExercises;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MethodExerciseTest {
+
+  @Test
+  void caculateScore() {
+  }
+
+  @Test
+  void negativeScore() {
+  }
+
+}
