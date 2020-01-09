a45c466c927a62f45e7078773f3d1801246f8d88
hamnord <h.nordenstein@hotmail.com>
2020-01-09 22:02:35 +0100

    finished bark challenge

--- src/JavaExercises/BarkingDogExercise.java
+++ src/JavaExercises/BarkingDogExercise.java
@@ -1,18 +1,26 @@
 package JavaExercises;
 
-import javax.swing.*;
 
 public class BarkingDogExercise {
 
   public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-    if (barking == hourOfDay < 8 || hourOfDay <= 22) {
-      System.out.println("The Dog barks");
-      return true;
-    } else if (hourOfDay > 23)
-      System.out.println("Invalid Range");
+
+    if (hourOfDay < 0 || hourOfDay > 23) {
+      System.out.println("invalid Range");
       return false;
+
+    } else if (hourOfDay >= 8 && hourOfDay <= 22) {
+      System.out.println("false");
+      return false;
+
+    } else if (hourOfDay >= 1 || hourOfDay <= 7 || hourOfDay >= 23) {
+      if (barking == true) {
+        System.out.println("The Dog barks");
+        return true;
+      }
+      System.out.println("not Barking");
+      return false;
+    }
+    return false;
   }
-}
-
-
-
+  }
