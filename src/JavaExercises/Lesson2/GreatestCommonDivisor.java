fcd086c1755c90077df3a0dc5bc288066362e6ae
hamnord <h.nordenstein@hotmail.com>
2020-02-08 16:34:40 +0100

    added a finished challenge

--- src/JavaExercises/Lesson2/GreatestCommonDivisor.java
+++ src/JavaExercises/Lesson2/GreatestCommonDivisor.java
@@ -1,4 +1,19 @@
 package JavaExercises.Lesson2;
 
 public class GreatestCommonDivisor {
+  public static int getGreatestCommonDivisor (int i, int j){
+    if ( i < 10 || j < 10){
+      System.out.println("Invalid Value");
+      return -1;
+    }
+    int common = 0;
+
+    for (int y = 1; y <= i && y <= j ; y++){
+      if ( i % y == 0 && j % y == 0){
+        common = y;
+      }
+    }
+    System.out.println("CommonDivisor: " + common);
+    return common;
+  }
 }
